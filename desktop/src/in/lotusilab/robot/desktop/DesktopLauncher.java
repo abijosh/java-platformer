9cf5ba6be30d72ab95576912b1877e509cfe5aec
abijosh <abijosh@gmail.com>
2017-03-25 10:57:17 +0000

    Initial commit

---
+++ desktop/src/in/lotusilab/robot/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package in.lotusilab.robot.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import in.lotusilab.robot.RobotGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RobotGame(), config);
+	}
+}
---
+++ ios/src/in/lotusilab/robot/IOSLauncher.java
@@ -0,0 +1,22 @@
+package in.lotusilab.robot;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import in.lotusilab.robot.RobotGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new RobotGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
